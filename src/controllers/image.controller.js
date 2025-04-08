0d8033e94e58f09bf63b1d4cb38e70b2ff93ac02
Hamailkhan <hamailkhan213@gmail.com>
2025-04-08 13:04:34 -0700

    create upload store image api and setup cloudinary

--- src/controllers/image.controller.js
+++ src/controllers/image.controller.js
@@ -1,9 +1,97 @@
-const uploadImage = async (req, res) => {
-  return res.status(200).json({
-    message: "Image uploaded successfully",
-  });
+const fs = require("fs");
+const cloudinary = require("../config/cloudinary.config");
+
+// const uploadImage = async (req, res) => {
+//   try {
+//     const { folder } = req.body;
+
+//     const uploadResult = await cloudinary.uploader.upload(req.file.path, {
+//       public_id: `${req.file.filename}`,
+//       folder: folder,
+//     });
+
+//     fs.unlinkSync(req.file.path);
+
+//     return res.status(200).json({
+//       success: true,
+//       message: "Image uploaded successfully!",
+//       url: uploadResult.secure_url,
+//       public_id: uploadResult.public_id,
+//     });
+//   } catch (error) {
+//     return res.status(500).json({
+//       success: false,
+//       message: "Image upload failed",
+//       error: error.message,
+//     });
+//   }
+// };
+
+const uploadStoreImage = async (req, res) => {
+  try {
+    let imageData = {};
+
+    if (req.files.storeLogo) {
+      const file = req.files.storeLogo[0];
+      const result = await cloudinary.uploader.upload(file.path, {
+        public_id: `${file.filename}`,
+        folder: "storesLogos",
+      });
+      fs.unlinkSync(file.path);
+      imageData.storeLogo = {
+        url: result.secure_url,
+        public_id: result.public_id,
+      };
+    }
+
+    if (req.files.storeCover) {
+      const file = req.files.storeCover[0];
+      const result = await cloudinary.uploader.upload(file.path, {
+        public_id: `${file.filename}`,
+        folder: "storesCoverPhotos",
+      });
+      fs.unlinkSync(file.path);
+      imageData.storeCover = {
+        url: result.secure_url,
+        public_id: result.public_id,
+      };
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Images uploaded successfully",
+      images: imageData,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Image upload failed",
+      error: error.message,
+    });
+  }
+};
+
+const optimizeImage = async (req, res) => {
+  try {
+    const { publicId } = req.query;
+
+    const optimizedUrl = cloudinary.url(publicId, {
+      fetch_format: "auto",
+      quality: "auto",
+    });
+
+    return res.status(200).json({
+      success: true,
+      optimized_url: optimizedUrl,
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, error: "Image optimization failed" });
+  }
 };
 
 module.exports = {
-  uploadImage,
+  uploadStoreImage,
+  optimizeImage,
 };
--- src/routes/image.routes.js
+++ src/routes/image.routes.js
@@ -1,8 +1,32 @@
+const multer = require("multer");
 const express = require("express");
-const { uploadImage } = require("../controllers/image.controller");
+const {
+  uploadStoreImage,
+  optimizeImage,
+} = require("../controllers/image.controller");
 
 const route = express.Router();
 
-route.post("/upload", uploadImage);
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "src/uploads/"); // Adjust path as needed
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname); // Unique filename
+  },
+});
+
+const upload = multer({ storage: storage });
+
+route.post(
+  "/upload-store-image",
+  upload.fields([
+    { name: "storeLogo", maxCount: 1 },
+    { name: "storeCover", maxCount: 1 },
+  ]),
+  uploadStoreImage
+);
+// route.get("/upload", uploadImage);
+route.get("/optimize", optimizeImage);
 
 module.exports = { route };
